66205b3662ac95e3552e4f2ac15aa974c39f93f7
Oleg Zablotskyi <Zablotsky@gmail.com>
2019-11-07 17:41:34 -0500

    last commit

--- src/App.js
+++ src/App.js
@@ -8,21 +8,44 @@ class App extends Component {
     userName: 'Krishna Bharat'
   };
   
+  switchNameHandler = (newName) => {
+    this.setState({
+      userName: newName
+    });
+  }
+
   nameChangedHandler = (event) => {
     this.setState({
       userName: event.target.value
-    })
+    });
   }
 
-  render() {
+
+
+  render(){
+
+    const style = {
+      backgroundColor: 'white',
+      font: 'inherit',
+      border: '1px solid blue',
+      padding: '8px',
+      cursor: 'pointer',
+      display: 'inline-block'
+    };
+
+
     return (
-      <div className="App">
-        <h1>Hi, I'm a React App</h1> 
+      <div className="App" style={style}>
+        <h1>Hi, I'm yor First App</h1> 
         <p>This is really working!</p>
         <UserOutput 
-        username={this.state.userName}/>
+        username={this.state.userName}
+        />
+        <UserInput
+        changed={this.nameChangedHandler}/>
         <UserOutput
-        username={this.state.userName} />
+        username={this.state.userName}
+        click={this.switchNameHandler.bind(this, 'Bency Sebastian ')} />
       </div>
     );
   }
--- src/UserOutput/UserOutput.js
+++ src/UserOutput/UserOutput.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import './UserOutput.css';
 
 const userOutput = (props) => {
     return(
         <div className="UserOutput">
             <p>Google News is a news aggregator app developed by Google. It presents a continuous, customizable flow of articles organized from thousands of publishers and magazines. Google News is available as an app on Android, iOS, and the Web.</p>
-            <p>Google released a beta version in September 2002 and the official app in January 2006.[1] The initial idea was developed by {props.username}.</p>
+            <p onClick={props.click}>Google released a beta version in September 2002 and the official app in January 2006.[1] The initial idea was developed by {props.username}.</p>
         </div>
     );
 }
