9c25a85afe0c19726a5cb54de6f00dbbc5c5441c
Oleg Zablotskyi <zablotsky@gmail.com>
2019-11-06 23:31:35 -0500

    added a state to app.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React, { Component } from 'react';
 import './App.css';
+import UserOutput from './UserOutput/UserOutput';
+import UserInput from './UserInput/UserInput';
 
 class App extends Component {
+  state = {
+    name: 'Krishna Bharat'
+  };
+  
   render() {
     return (
       <div className="App">
-        
+        <h1>Hi, I'm a React App</h1> 
+        <p>This is really working!</p>
+        <UserOutput 
+        userName={this.state.name}/>
+        <UserOutput />
       </div>
     );
   }
